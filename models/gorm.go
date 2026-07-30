5c39a8c6136d0c22cb9472d4c4d1f36bb8175267
Zeeshan Ahmed <zazeeshan@1678gmail.com>
2026-07-30 14:26:02 +0500

    Refactor models: clean up comments and improve code readability in User and Message structs

--- models/gorm.go
+++ models/gorm.go
@@ -4,24 +4,18 @@ import (
 	"gorm.io/gorm"
 )
 
-// User table
+
 type User struct {	
-	gorm.Model // ID, CreatedAt, UpdatedAt, DeletedAt
+	gorm.Model 
 
 	Name     string `json:"name" gorm:"not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 }
 
-// // Login request (not a database table)
-// type Login struct {
-// 	Email    string `json:"email"`
-// 	Password string `json:"password"`
-// }
 
-// Message table
 type Message struct {
-	gorm.Model // ID, CreatedAt, UpdatedAt, DeletedAt
+	gorm.Model 
 
 	SenderID   uint   `json:"sender_id" gorm:"not null"`
 	ReceiverID uint   `json:"receiver_id" gorm:"not null"`
